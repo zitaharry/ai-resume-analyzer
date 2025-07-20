841ba22b7e4e272e2e3b7a84706fbbe49727bdb6
zitaharry <harrymuigua@gmail.com>
2025-07-20 12:46:21 +0300

    Implement Cloud storagem PDF to Image conversion, Key-Value storage and AI feedback

---
+++ app/routes/resume.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate, useParams } from "react-router";
+
+export const meta = () => [
+  { title: "Resumind | Review" },
+  { name: "description", content: "Detailed overview of your resume" },
+];
+
+const Resume = () => {
+  const { auth, isLoading, fs, kv } = usePuterStore();
+  const { id } = useParams();
+  const [imageUrl, setImageUrl] = useState("");
+  const [resumeUrl, setResumeUrl] = useState("");
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !auth.isAuthenticated)
+      navigate(`/auth?next=/resume/${id}`);
+  }, [isLoading]);
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const resume = await kv.get(`resume:${id}`);
+
+      if (!resume) return;
+
+      const data = JSON.parse(resume);
+
+      const resumeBlob = await fs.read(data.resumePath);
+      if (!resumeBlob) return;
+
+      const pdfBlob = new Blob([resumeBlob], { type: "application/pdf" });
+      const resumeUrl = URL.createObjectURL(pdfBlob);
+      setResumeUrl(resumeUrl);
+
+      const imageBlob = await fs.read(data.imagePath);
+      if (!imageBlob) return;
+      const imageUrl = URL.createObjectURL(imageBlob);
+      setImageUrl(imageUrl);
+
+      setFeedback(data.feedback);
+      console.log({ resumeUrl, imageUrl, feedback: data.feedback });
+    };
+
+    loadResume();
+  }, [id]);
+
+  return <div>Resume</div>;
+};
+export default Resume;
